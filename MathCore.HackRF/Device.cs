cc72227be264caa4bf3c0e465bd6590351001f35
Infarh <shmachilin@gmail.com>
2025-08-10 14:10:36 +0300

    Добавлены свойства устройства HackRF

--- MathCore.HackRF/Device.cs
+++ MathCore.HackRF/Device.cs
@@ -1,7 +1,10 @@
-﻿namespace MathCore.HackRF;
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace MathCore.HackRF;
 
 /// <summary>Класс управления устройством HackRF</summary>
-public partial class Device
+public partial class Device : IDisposable
 {
     /// <summary>Создаёт объект управления устройством HackRF. Открывает первое доступное устройство и получает его серийный номер.</summary>
     public Device()
@@ -35,4 +38,39 @@ public partial class Device
 #else
     private readonly Lock _SyncRoot = new();
 #endif
+
+    /* --------------------------------------------------------------------------------------- */
+
+    /// <summary>Деструктор, освобождающий ресурсы устройства</summary>
+    ~Device() => Dispose();
+
+    /// <summary>Флаг, указывающий, был ли объект освобождён</summary>
+    private bool _Disposed;
+
+    /// <summary>Освобождение ресурсов устройства.</summary>
+    public void Dispose()
+    {
+        if (_Disposed) return;
+
+        lock (_SyncRoot)
+        {
+            if (_Disposed) return;
+
+            _Disposed = true;
+            GC.SuppressFinalize(this);
+
+            var err_close = HackRFLib.Close(DevicePtr);
+            if (err_close != HackRfError.Success)
+                throw new InvalidOperationException($"Ошибка закрытия устройства: {err_close}")
+                    .WithData(nameof(DevicePtr), DevicePtr)
+                    .WithData(nameof(SerialNumber), SerialNumber ?? "")
+                    .WithData(nameof(err_close), err_close);
+            Trace.TraceInformation($"Закрыта плата HackRFOne sn:{SerialNumber} ptr:{DevicePtr:x}");
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void ThrowIfDisposed() => ObjectDisposedException.ThrowIf(_Disposed, this);
+
+
 }
