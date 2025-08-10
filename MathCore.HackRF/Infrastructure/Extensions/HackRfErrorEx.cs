ca74d62baff9dd18be22f5937fe1e4269cf29ae2
Infarh <shmachilin@gmail.com>
2025-08-10 14:42:01 +0300

    Запуск и остановка режимов приёма и передачи

---
+++ MathCore.HackRF/Infrastructure/Extensions/HackRfErrorEx.cs
@@ -0,0 +1,10 @@
+﻿namespace MathCore.HackRF.Infrastructure.Extensions;
+
+internal static class HackRfErrorEx
+{
+    public static void ThrowIfNotSuccess(this HackRfError error, string Message)
+    {
+        if (error == HackRfError.Success) return;
+        throw new InvalidOperationException($"{Message}. Код: {error}");
+    }
+}
